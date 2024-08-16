7dfff8cceb501cdc8c34cad228d5d7d7870d65e4
Alexander Nortung <alexander.nortung@oakdigital.dk>
2024-08-16 18:49:47 +0200

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,50 @@
 import { defineConfig } from 'vite'
+import replace from 'rollup-plugin-replace';
 import react from '@vitejs/plugin-react'
+import externalize from 'vite-plugin-externalize-dependencies';
+
+const external = [
+  // 'react',
+  // 'react-dom',
+  // '@builder.io/react',
+  '@builder.io/app-context',
+  // '@builder.io/commerce-plugin-tools/dist/types/interfaces/resource',
+];
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // plugins: [react()],
+  plugins: [
+    react(),
+    externalize({ externals: external }),
+  ],
   build: {
     lib: {
       entry: 'src/plugin.ts',
-      // name: 'MyLib',
+      name: 'BuilderPluginMedusa',
       // fileName: (format) => `plugin.${format}.js`,
       fileName: 'index',
-      formats: ['cjs', 'es']
+      formats: ['cjs', 'es', 'iife', 'umd'],
     },
     rollupOptions: {
+      preserveEntrySignatures: 'strict',
       output: {
         format: 'system',
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          '@builder.io/app-context': 'appState',
+        },
       },
-      external: [
-        'react',
-        'react-dom',
-        '@builder.io/react',
-        '@builder.io/app-context',
-        '@builder.io/commerce-plugin-tools/dist/types/interfaces/resource',
+      external,
+      // external: {
+      //   react: 'React',
+      //   'react-dom': 'ReactDOM',
+      //   '@builder.io/app-context': 'appState',
+      // },
+      plugins: [
+        replace({
+          'process.env.NODE_ENV': JSON.stringify('production'),
+        }),
       ],
     },
   },
